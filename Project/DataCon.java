92b8a1df204260d2a544cab23cac47a0f6edc706
Anahita sodhi <124506694+Anahita-sodhi@users.noreply.github.com>
2025-07-12 18:47:55 +0530

    Update DataCon.java

--- Project/DataCon.java
+++ Project/DataCon.java
@@ -11,6 +11,7 @@ public static Connection getCon() {
 	try {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 	con = DriverManager.getConnection("jdbc:mysql://localhost:3307/elibrary","root","1234");
+		System.out.println("Connection established successfully");
 	}
 	catch(Exception e) {
 		e.printStackTrace();
